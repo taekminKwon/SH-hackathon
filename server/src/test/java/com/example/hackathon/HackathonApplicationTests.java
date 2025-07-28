4fee11a477c85b82acbc785b7413f49e031fb611
taekminKwon <rnjsxorals1@gmail.com>
2025-07-28 16:21:57 +0900

    Settings : 초기 세팅

---
+++ server/src/test/java/com/example/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class HackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
