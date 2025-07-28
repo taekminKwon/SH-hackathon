7b0f146e95536922944a93a12d0dbbcf738319c5
taekMinKwon <rnjsxorals1@gmail.com>
2025-07-28 22:46:49 +0900

    Setting : SuccessCode 일부 수정

--- server/src/main/java/com/example/hackathon/common/SuccessCode.java
+++ server/src/main/java/com/example/hackathon/common/SuccessCode.java
@@ -12,23 +12,7 @@ public enum SuccessCode {
      */
     OK(HttpStatus.OK, "요청 성공"),
     UPDATED(HttpStatus.OK, "수정 완료"),
-    DELETED(HttpStatus.OK, "삭제 완료"),
-
-    /*
-       201 Created
-     */
-    CREATED(HttpStatus.CREATED, "생성 완료"),
-
-    /*
-        203 Multi-Status
-     */
-    MULTISTATUS(HttpStatus.MULTI_STATUS, "일부만 처리에 성공했습니다."),
-
-    /*
-       204 No Content
-     */
-    NO_CONTENT(HttpStatus.NO_CONTENT, "요청 성공(반환 데이터 없음)");
-
+    DELETED(HttpStatus.OK, "삭제 완료");
     private final HttpStatus status;
     private final String message;
 
