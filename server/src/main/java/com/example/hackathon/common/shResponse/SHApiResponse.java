bc3c43cfab76736704f0212fce99acfd8cda7626
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 00:56:37 +0900

    feat : SHApiResponse JsonProperty 수정

--- server/src/main/java/com/example/hackathon/common/shResponse/SHApiResponse.java
+++ server/src/main/java/com/example/hackathon/common/shResponse/SHApiResponse.java
@@ -1,5 +1,6 @@
 package com.example.hackathon.common.shResponse;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SHApiResponse<T> {
+    @JsonProperty("Header")
     private SHHeader.Response header;
+    @JsonProperty("REC")
     private T responseBody;
 
     @Builder
