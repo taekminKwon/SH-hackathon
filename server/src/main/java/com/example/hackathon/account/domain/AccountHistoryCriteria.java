7e2a574e9fe837cb78913e3a02244246b88dc3e0
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 14:19:21 +0900

    Feature : 유저 통신 dto 개발

---
+++ server/src/main/java/com/example/hackathon/account/domain/AccountHistoryCriteria.java
@@ -0,0 +1,9 @@
+package com.example.hackathon.account.domain;
+
+public class AccountHistoryCriteria {
+    private String accountNo;
+    private String startDate;
+    private String endDate;
+    private String transactionType;
+    private String orderByType;
+}
---
+++ server/src/main/java/com/example/hackathon/account/domain/AccountSummaryCriteria.java
@@ -0,0 +1,10 @@
+package com.example.hackathon.account.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class AccountSummaryCriteria {
+    private String accountNo;
+}
