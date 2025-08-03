a01c5b38812cb1a4d90a38a4588131813038466f
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 15:32:24 +0900

    Feature : 계정 조회 API 추가

--- server/src/main/java/com/example/hackathon/user/infrastructure/SHMockUserAccountAdapter.java
+++ server/src/main/java/com/example/hackathon/user/infrastructure/SHMockUserAccountAdapter.java
@@ -1,9 +1,9 @@
 package com.example.hackathon.user.infrastructure;
 
 import com.example.hackathon.user.domain.SHDto.SHUserAccountCommand;
+import com.example.hackathon.user.domain.SHDto.SHUserAccountCriteria;
 import com.example.hackathon.user.domain.SHDto.SHUserAccountInfo;
 import com.example.hackathon.user.domain.SHUserAccountPort;
-import com.example.hackathon.user.domain.SHUserAccountStorePort;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -13,13 +13,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SHMockUserAccountAdapter implements SHUserAccountPort {
-    private static final Map<String, String> db = new ConcurrentHashMap<>();
+    private static final Map<String, SHUserAccountInfo> db = new ConcurrentHashMap<>();
 
     @Override
     public SHUserAccountInfo createUserAccount(SHUserAccountCommand command) {
         String userUUID = UUID.randomUUID().toString();
-        db.put(command.getUserId(), userUUID);
-        return SHUserAccountInfo.builder()
+        SHUserAccountInfo info = SHUserAccountInfo.builder()
                 .userId(command.getUserId())
                 .userName(command.getUserId().substring(0, command.getUserId().indexOf("@")))
                 .institutionCode("00100")
@@ -27,5 +26,17 @@ public class SHMockUserAccountAdapter implements SHUserAccountPort {
                 .created(LocalDateTime.now().toString())
                 .modified(LocalDateTime.now().toString())
                 .build();
+        if (!db.containsKey(info.getUserId())) {
+            db.put(command.getUserId(), info);
+        } else {
+            throw new IllegalArgumentException("이미 있는 사용자입니다");
+        }
+
+        return info;
+    }
+
+    @Override
+    public SHUserAccountInfo getUserAccount(SHUserAccountCriteria criteria) {
+        return db.get(criteria.getUserId());
     }
 }
