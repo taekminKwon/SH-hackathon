34423688227d82de51bff61a02a4de52a8ec3afd
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 15:32:33 +0900

    Feature : Controller 기능 추가

--- server/src/main/java/com/example/hackathon/user/interfaces/UserTestController.java
+++ server/src/main/java/com/example/hackathon/user/interfaces/UserTestController.java
@@ -1,15 +1,13 @@
 package com.example.hackathon.user.interfaces;
 
 import com.example.hackathon.user.domain.SHDto.SHUserAccountCommand;
+import com.example.hackathon.user.domain.SHDto.SHUserAccountCriteria;
 import com.example.hackathon.user.domain.SHDto.SHUserAccountInfo;
 import com.example.hackathon.user.domain.SHUserAccountPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +20,12 @@ public class UserTestController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(port.createUserAccount(command));
+    }
+
+    @GetMapping
+    public ResponseEntity<SHUserAccountInfo> getUserInfo(@RequestBody SHUserAccountCriteria criteria) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(port.getUserAccount(criteria));
     }
 }
