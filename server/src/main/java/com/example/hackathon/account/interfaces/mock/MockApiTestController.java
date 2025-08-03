b1cb91a7dac29dd6435a49b5d67c829dea655602
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 14:20:08 +0900

    Feature : 테스트 컨트롤러 생성

---
+++ server/src/main/java/com/example/hackathon/account/interfaces/mock/MockApiTestController.java
@@ -0,0 +1,39 @@
+package com.example.hackathon.account.interfaces.mock;
+
+import com.example.hackathon.account.domain.AccountHistoryCriteria;
+import com.example.hackathon.account.domain.AccountReaderPort;
+import com.example.hackathon.account.domain.AccountSummaryCriteria;
+import com.example.hackathon.account.domain.shResponse.SHAccountREC;
+import com.example.hackathon.account.domain.shResponse.SHTransactionHistoryREC;
+import com.example.hackathon.common.shDto.SHApiRECResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class MockApiTestController {
+    private final AccountReaderPort accountReaderPort;
+    @GetMapping("/getAccount")
+    public ResponseEntity<SHApiRECResponse<SHAccountREC>> testGetAccount(
+            @RequestBody AccountSummaryCriteria criteria
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountReaderPort.getAccountSummary(criteria));
+    }
+
+    @GetMapping("/getHistories")
+    public ResponseEntity<SHApiRECResponse<SHTransactionHistoryREC>> testGetHistory(
+            @RequestBody AccountHistoryCriteria criteria
+        ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(accountReaderPort.getAccountTransactionHistory(criteria));
+    }
+}
