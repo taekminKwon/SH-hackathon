7976629b988ef99020e562018b85328e09d826a9
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 15:04:19 +0900

    Feature : 외부 API 통신 객체 생성

---
+++ server/src/main/java/com/example/hackathon/user/domain/SHDto/SHUserAccountCommand.java
@@ -0,0 +1,10 @@
+package com.example.hackathon.user.domain.SHDto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class SHUserAccountCommand {
+    private String userId;
+}
