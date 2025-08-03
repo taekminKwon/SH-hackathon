bc2f53a3a745cbb5c439d0def2d656bb7aefe190
taekMinKwon <rnjsxorals1@gmail.com>
2025-08-03 15:16:56 +0900

    Feature : 인메모리 db에 데이터 저장

--- server/src/main/java/com/example/hackathon/user/infrastructure/SHMockUserAccountAdapter.java
+++ server/src/main/java/com/example/hackathon/user/infrastructure/SHMockUserAccountAdapter.java
@@ -2,22 +2,28 @@ package com.example.hackathon.user.infrastructure;
 
 import com.example.hackathon.user.domain.SHDto.SHUserAccountCommand;
 import com.example.hackathon.user.domain.SHDto.SHUserAccountInfo;
+import com.example.hackathon.user.domain.SHUserAccountPort;
 import com.example.hackathon.user.domain.SHUserAccountStorePort;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-public class SHMockUserAccountStoreAdapter implements SHUserAccountStorePort {
+public class SHMockUserAccountAdapter implements SHUserAccountPort {
+    private static final Map<String, String> db = new ConcurrentHashMap<>();
 
     @Override
     public SHUserAccountInfo createUserAccount(SHUserAccountCommand command) {
+        String userUUID = UUID.randomUUID().toString();
+        db.put(command.getUserId(), userUUID);
         return SHUserAccountInfo.builder()
                 .userId(command.getUserId())
                 .userName(command.getUserId().substring(0, command.getUserId().indexOf("@")))
                 .institutionCode("00100")
-                .userKey(UUID.randomUUID().toString())
+                .userKey(userUUID)
                 .created(LocalDateTime.now().toString())
                 .modified(LocalDateTime.now().toString())
                 .build();
