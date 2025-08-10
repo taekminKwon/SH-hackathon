171ad91d007dca1ff70c1231b54f265370e6af36
Mseunghwan <minccc42710@gmail.com>
2025-08-10 16:05:01 +0900

    feat : SecurityConfig 설정

---
+++ server/src/main/java/com/example/hackathon/common/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.hackathon.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    // 비밀번호 암호화 객체를 Spring 컨테이너에 Bean 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 보안 규칙 설정
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // REST API 서버는 stateless --> csrf, session disable
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 회원가입, 로그인에 필요한 '/api/auth' 로 시작하는 요청은 인증없이 허용, 아니면 인증 거쳐야
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                // 추후 JWT 인증필터 추가해야
+                .build();
+    }
+
+}
